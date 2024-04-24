1211c0d7bba625d9ece00690592a01667692acd9
Zilkod <83935849+Zilkod@users.noreply.github.com>
2024-04-24 17:38:04 +0100

    Assignment 7: Python Basic Assignment

---
+++ PythonBasicAssignments/Assignment_7/Question1.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print(f"First variable is {num1} & second variable is {num2}.")
+print(f"Addition: {num1} + {num2} = {add}")
+print(f"Subtraction: {num1} - {num2} = {sub}")
+print(f"Multiplication: {num1} * {num2} = {mul}")
+print(f"Division: {num1} / {num2} = {div}")
---
+++ PythonBasicAssignments/Assignment_7/Question5.py
@@ -0,0 +1,6 @@
+my_list = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+
+if 10 in my_list:
+    print("10 is present in the list.")
+else:
+    print("10 is not present in the list.")
